45e3d21aee6de1bef62aa9a27a0386126218c5eb
João Alencar <joao.alencar1497@gmail.com>
2022-11-15 15:13:44 -0300

    refatorado codigo, separando serviço do gihub do componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,11 +14,12 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,29 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { GithubServices } from 'src/app/services/github-services';
 import { IRepositorio } from 'src/interfaces/IRepositorio';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+@Injectable()
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private githubServices: GithubServices) { }
 
-  private urlRepo = "https://api.github.com/users/Joaoalen98/repos";
   public repositorios: IRepositorio[] = [];
 
   ngOnInit(): void {
-    this.getRepositorios();
-  }
-
-  getRepositorios(): void {
-    fetch(this.urlRepo)
-      .then(res => res.json())
-      .then((res: IRepositorio[]) => {
-        this.repositorios = res.filter(rep => rep.name !== "Joaoalen98");
-      })
-      .catch(err => console.log(err));
+    this.githubServices.getRepos()
+      .subscribe(reps =>
+        this.repositorios = reps.filter(reps => reps.name != "Joaoalen98"));
   }
 }
